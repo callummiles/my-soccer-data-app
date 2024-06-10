0ea22cbc799ce6ef8b6c24d4077027e5662e5eb9
callummiles <cwmiles18@gmail.com>
2024-06-10 18:24:14 -0500

    adding selections to model

--- server/models/MarketModel.js
+++ server/models/MarketModel.js
@@ -5,12 +5,12 @@ const basePath = `/api/rest/v2/namespaces/testks/collections/markets`;
 export const insertMarket = async (market) => {
   try {
     const client = await astraClient;
-    console.log('Market ID;', typeof market.id);
-    console.log('Market Status;', typeof market.status);
-    console.log('Market Timestamp:', typeof market.lastUpdated);
-    console.log('Market inPlay:', typeof market.inPlay);
-    console.log('Market inPlayTime:', market.inPlayTime);
-    console.log('Market Volume:', typeof market.volume);
+    // console.log('Market ID;', typeof market.id);
+    // console.log('Market Status;', typeof market.status);
+    // console.log('Market Timestamp:', typeof market.lastUpdated);
+    // console.log('Market inPlay:', typeof market.inPlay);
+    // console.log('Market inPlayTime:', market.inPlayTime);
+    // console.log('Market Volume:', typeof market.volume);
     const marketData = {
       id: market.id,
       status: market.status,
@@ -18,16 +18,16 @@ export const insertMarket = async (market) => {
       inPlay: market.inPlay,
       inPlayTime: market.inPlayTime,
       volume: market.volume,
-      // selections: market.selections.map((selection) => ({
-      //   [selection.id]: {
-      //     'lay1.prc': selection.lay1 ? selection.lay1.prc : null,
-      //     'lay1.sz': selection.lay1 ? selection.lay1.sz : null,
-      //     'back1.prc': selection.back1 ? selection.back1.prc : null,
-      //     'back1.sz': selection.back1 ? selection.back1.sz : null,
-      //     LTP: selection.LTP,
-      //     vol: selection.vol,
-      //   },
-      // })),
+      selections: market.selections.map((selection) => ({
+        [selection.id]: {
+          'lay1.prc': selection.lay1 ? selection.lay1.prc : null,
+          'lay1.sz': selection.lay1 ? selection.lay1.sz : null,
+          'back1.prc': selection.back1 ? selection.back1.prc : null,
+          'back1.sz': selection.back1 ? selection.back1.sz : null,
+          LTP: selection.LTP,
+          vol: selection.vol,
+        },
+      })),
     };
 
     console.log('Market Data: ', JSON.stringify(marketData, null, 2));
