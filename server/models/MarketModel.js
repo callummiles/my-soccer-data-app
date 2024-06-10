c5e7a35fca76f1f2455f1138662f03a4d391a07e
callummiles <cwmiles18@gmail.com>
2024-06-10 16:20:27 -0500

    updated market model shape

--- server/models/MarketModel.js
+++ server/models/MarketModel.js
@@ -5,7 +5,29 @@ const basePath = `/api/rest/v2/namespaces/testks/collections/markets`;
 export const insertMarket = async (market) => {
   try {
     const client = await astraClient;
-    const response = await client.post(basePath, market);
+    const marketData = {
+      id: market.id,
+      status: market.status,
+      lastUpdated: market.lastUpdated,
+      inPlay: market.inPlay,
+      inPlayTime: market.inPlayTime,
+      volume: market.volume,
+      selections: market.selections.map((selection) => ({
+        id: selection.id,
+        lay1: selection.lay1
+          ? { prc: selection.lay1.prc, sz: selection.lay1.sz }
+          : null,
+        back1: selection.back1
+          ? { prc: selection.back1.prc, sz: selection.back1.sz }
+          : null,
+        LTP: selection.LTP,
+        vol: selection.vol,
+      })),
+    };
+
+    console.log('Market Data: ', JSON.stringify(marketData, null, 2));
+
+    const response = await client.post(basePath, marketData);
     return response.data;
   } catch (e) {
     console.error(e);
