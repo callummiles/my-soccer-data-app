c6700616cb5eeba89c1eb75cd07987edcbbe0a68
callummiles <cwmiles18@gmail.com>
2024-06-11 18:41:20 -0500

    updating db connection

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@
 import express from 'express';
 import ViteExpress from 'vite-express';
 import marketRoutes from './routes/marketRoutes.js';
-import { astraClient } from './config/astraClient.js';
-import { createTable } from './config/createTable.js';
+import client from './config/astraClient.js';
+//import { createTable } from './config/createTable.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -11,13 +11,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-createTable()
-  .then(() => {
-    console.log('Table creation complete.');
-  })
-  .catch((err) => {
-    console.log('Error creating table: ', err);
-  });
+// createTable()
+//   .then(() => {
+//     console.log('Table creation complete.');
+//   })
+//   .catch((err) => {
+//     console.log('Error creating table: ', err);
+//   });
 
 app.use('/api', marketRoutes);
 
@@ -25,7 +25,7 @@ app.get('/message', (_, res) => res.send('Hello from express!'));
 
 ViteExpress.listen(app, 3000, async () => {
   try {
-    const client = await astraClient;
+    await client.connect();
     console.log('Astra DB client initialized: ', client.baseUrl);
     console.log('Server is listening...');
   } catch (e) {
