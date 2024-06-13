765e08968bc84c0aca628c3d0d9fcc99328c87d0
callummiles <cwmiles18@gmail.com>
2024-06-12 22:15:28 -0500

    filtering out closed markets

--- server/controllers/marketController.js
+++ server/controllers/marketController.js
@@ -15,8 +15,16 @@ export const fetchOnce = async (req, res) => {
       console.log('Market data already cached.');
     }
 
-    await insertDataInDB(data);
-    res.status(200).send('Data fetched and stored.');
+    const filteredMarkets = data.result.markets.filter(
+      (market) => market.status !== 'CLOSED'
+    );
+
+    if (filteredMarkets.length > 0) {
+      await insertDataInDB({ result: { markets: filteredMarkets } });
+      res.status(200).send('Data fetched and stored.');
+    } else {
+      res.status(200).send('No open markets to store.');
+    }
   } catch (e) {
     res.status(500).send(`Error fetching or storing data: ${e.message}`);
   }
@@ -52,7 +60,7 @@ export const fetchInterval = (req, res) => {
         console.log(`Fetching data for market ${market.id} at ${new Date()}`);
         const data = await fetchData();
         const marketData = data.result.markets.find((m) => m.id === market.id);
-        if (marketData) {
+        if (marketData && marketData.status !== 'CLOSED') {
           await insertDataInDB({ result: { markets: [marketData] } });
           console.log(
             `Data fetched and stored successfully for market ${market.id}.`
