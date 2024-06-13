8eef63d1547baa9bbc84e0ccaf7317a42c3e8e7c
callummiles <cwmiles18@gmail.com>
2024-06-13 11:39:27 -0500

    added immediate fetch on intervals

--- server/controllers/marketController.js
+++ server/controllers/marketController.js
@@ -80,19 +80,7 @@ export const fetchInterval = (req, res) => {
       console.log(
         `Fetch start time for market ${market.id} is in the past. Starting interval immediately.`
       );
-      // const intID = setInterval(async () => {
-      //   try {
-      //     console.log(`Fetching data for market ${market.id} at ${now}`);
-      //     const data = await fetchData();
-      //     await insertDataInDB(data);
-      //     console.log(`Data fetched successfully for market ${market.id}`);
-      //   } catch (e) {
-      //     console.error(
-      //       `Error fetching data for market ${market.id}: `,
-      //       e.message
-      //     );
-      //   }
-      // }, interval);
+      fetchMarketData(market);
       const intID = setInterval(fetchMarketData, interval);
 
       intervalMap.set(market.id, intID);
@@ -101,19 +89,7 @@ export const fetchInterval = (req, res) => {
     } else {
       scheduleJob(fetchStartTime, () => {
         console.log(`Job started for market ${market.id} at ${new Date()}`);
-        // const intID = setInterval(async () => {
-        //   try {
-        //     console.log(`Fetching data for market ${market.id}`);
-        //     const data = await fetchData();
-        //     await insertDataInDB(data);
-        //     console.log(`Data fetched successfully for market ${market.id}`);
-        //   } catch (e) {
-        //     console.error(
-        //       `Error fetching data for market ${market.id}: `,
-        //       e.message
-        //     );
-        //   }
-        // }, interval);
+        fetchMarketData(market);
         const intID = setInterval(fetchMarketData, interval);
 
         intervalMap.set(market.id, intID);
