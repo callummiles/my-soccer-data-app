fdf10730fba8748154c57071c40840728a5672be
callummiles <cwmiles18@gmail.com>
2024-06-21 17:16:33 -0500

    undo prev commit

--- server/config/astraClient.js
+++ server/config/astraClient.js
@@ -1,20 +1,9 @@
 /* eslint-disable no-undef */
 import cassandra from 'cassandra-driver';
-import fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const {
-  ASTRA_DB_SECURE_BUNDLE_PATH,
-  ASTRA_DB_APP_TOKEN,
-  ASTRA_DB_SECURE_BUNDLE_B64,
-} = process.env;
-const bundlePath = ASTRA_DB_SECURE_BUNDLE_PATH;
-
-if (ASTRA_DB_SECURE_BUNDLE_B64 && !fs.existsSync(bundlePath)) {
-  const bundle = Buffer.from(ASTRA_DB_SECURE_BUNDLE_B64, 'base64');
-  fs.writeFileSync(bundlePath, bundle);
-}
+const { ASTRA_DB_SECURE_BUNDLE_PATH, ASTRA_DB_APP_TOKEN } = process.env;
 
 const cloud = { secureConnectBundle: ASTRA_DB_SECURE_BUNDLE_PATH };
 const authProvider = new cassandra.auth.PlainTextAuthProvider(
