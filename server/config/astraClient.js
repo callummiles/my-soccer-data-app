56a59cdc87219124d146e6d2f8b69b9a05e0e28c
callummiles <cwmiles18@gmail.com>
2024-06-24 13:19:20 -0500

    moving logs after spread

--- server/config/astraClient.js
+++ server/config/astraClient.js
@@ -3,10 +3,10 @@ import cassandra from 'cassandra-driver';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const { ASTRA_DB_SECURE_BUNDLE_PATH, ASTRA_DB_APP_TOKEN } = process.env;
+
 console.log(ASTRA_DB_APP_TOKEN);
 console.log(ASTRA_DB_SECURE_BUNDLE_PATH);
-
-const { ASTRA_DB_SECURE_BUNDLE_PATH, ASTRA_DB_APP_TOKEN } = process.env;
 
 const cloud = { secureConnectBundle: ASTRA_DB_SECURE_BUNDLE_PATH };
 const authProvider = new cassandra.auth.PlainTextAuthProvider(
