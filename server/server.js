ec8936d1e7d00332ab87b51f1e0822b51ed170c4
callummiles <cwmiles18@gmail.com>
2024-07-12 15:56:54 -0500

    clean up unneeded logs

--- server/server.js
+++ server/server.js
@@ -2,13 +2,11 @@
 import express from 'express';
 import ViteExpress from 'vite-express';
 import marketRoutes from './routes/marketRoutes.js';
-// import client from './config/astraClient.js';
 import promisedClient from './config/grpcConfig.js';
 import { Query } from '@stargate-oss/stargate-grpc-node-client';
 
 import dotenv from 'dotenv';
 import cors from 'cors';
-// import http from 'http';
 
 dotenv.config();
 
@@ -20,27 +18,8 @@ app.use(express.json());
 app.use('/api', marketRoutes);
 
 app.get('/message', (_, res) => {
-  // res.send('Hello from express!')
   res.json({ message: 'Hello from express!' });
 });
-
-// ViteExpress.listen(app, 3000, async () => {
-//   try {
-//     //await client.connect();
-//     const query = new Query();
-//     const queryString = 'SELECT * FROM bfex_data.markets LIMIT 1;';
-//     query.setCql(queryString);
-//     const result = await promisedClient.executeQuery(query);
-//     console.log(
-//       'Astra DB client initialized. Data: ',
-//       result.array[0][0][0][0][0]
-//     );
-//     console.log('Server is listening...');
-//   } catch (e) {
-//     console.error('Failed to initialize Astra DB client: ', e);
-//     process.exit(1);
-//   }
-// });
 
 (async () => {
   try {
