cf4b7680728782ed0196d5af3b1e12dee357c8d1
callummiles <cwmiles18@gmail.com>
2025-01-20 23:30:37 -0600

    fix: update vite proxy config to prevent double /api prefix

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,12 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
-      '/auth': 'http://localhost:3000',
-      '/message': {
+      '^/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '^/auth': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
