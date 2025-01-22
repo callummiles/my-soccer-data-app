d89b68f475777a8702e5a96159d130fa05777ce3
callummiles <cwmiles18@gmail.com>
2025-01-22 02:11:49 -0600

    refactor: update insertMarketInDB to use new Query format for Stargate OSS client

--- server/models/MarketModel.js
+++ server/models/MarketModel.js
@@ -2,66 +2,52 @@ import promisedClient from '../config/grpcConfig.js';
 import { Query } from '@stargate-oss/stargate-grpc-node-client';
 
 export const insertMarketInDB = async (market) => {
-  // console.log('[MarketModel] Starting insertMarketInDB...');
-  // console.log('[MarketModel] Market data:', JSON.stringify(market, null, 2));
+  console.log('[MarketModel] Starting insertMarketInDB...');
+  console.log('[MarketModel] Market data:', JSON.stringify(market, null, 2));
 
-  // try {
-  //   console.log('[MarketModel] Preparing values...');
-  //   const selectionsJSON = JSON.stringify(market.selections);
+  try {
+    console.log('[MarketModel] Preparing values...');
+    const selectionsJSON = JSON.stringify(market.selections);
+    
+    const query = new Query();
+    const queryString = `INSERT INTO bfex_data.markets (
+      market_id, status, last_updated, in_play, in_play_time,
+      volume, name, market_type, event_id, event_type_id,
+      selections, start_time, current_time, first_half_started,
+      first_half_ended, second_half_started, second_half_ended
+    ) VALUES (
+      '${market.id}', 
+      '${market.status}', 
+      ${new Date(market.lastUpdated).getTime()}, 
+      ${market.inPlay}, 
+      ${market.inPlayTime || 0}, 
+      ${market.volume || 0.0}, 
+      '${market.name}', 
+      '${market.marketType}', 
+      '${market.eventId}', 
+      '${market.eventTypeId}', 
+      '${selectionsJSON}', 
+      ${new Date(market.startTime).getTime()}, 
+      ${new Date().getTime()}, 
+      null, 
+      null, 
+      null, 
+      null
+    );`;
 
-  //   // Create a query builder for insertion
-  //   const query = {
-  //     query: {
-  //       value: `INSERT INTO markets (
-  //         market_id, status, last_updated, in_play, in_play_time,
-  //         volume, name, market_type, event_id, event_type_id,
-  //         selections, start_time, current_time, first_half_started,
-  //         first_half_ended, second_half_started, second_half_ended
-  //       ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-  //       parameters: {
-  //         values: [
-  //           { string: market.id },
-  //           { string: market.status },
-  //           { timestamp: new Date(market.lastUpdated).getTime() },
-  //           { boolean: market.inPlay },
-  //           { int: market.inPlayTime || 0 },
-  //           { double: market.volume || 0.0 },
-  //           { string: market.name },
-  //           { string: market.marketType },
-  //           { string: market.eventId },
-  //           { string: market.eventTypeId },
-  //           { string: selectionsJSON },
-  //           { timestamp: new Date(market.startTime).getTime() },
-  //           { timestamp: new Date().getTime() },
-  //           { boolean: null },
-  //           { boolean: null },
-  //           { boolean: null },
-  //           { boolean: null }
-  //         ]
-  //       }
-  //     }
-  //   };
+    // Set the CQL statement
+    query.setCql(queryString);
 
-  //   console.log('[MarketModel] Executing query...');
-  //   const response = await promisedClient.executeQuery(query);
-  //   console.log('[MarketModel] Query executed successfully:', response);
-  //   return response;
+    // Execute the query statement
+    const response = await promisedClient.executeQuery(query);
+    console.log('[MarketModel] Insert executed:', response);
+    return response;
 
-  // } catch (error) {
-  //   console.error('[MarketModel] Failed to insert market data:', error);
-  //   console.error('[MarketModel] Error stack:', error.stack);
-  //   throw error;
-  // }
-
-  const query = new Query();
-  const queryString = 'SELECT * FROM bfex_data.markets;';
-  // Set the CQL statement using the string defined in the last line
-  query.setCql(queryString);
-
-  // For Stargate OSS: execute the query statement
-  const response = await promisedClient.executeQuery(query);
-
-  console.log('[MarketModel] select executed:', response);
+  } catch (error) {
+    console.error('[MarketModel] Failed to insert market data:', error);
+    console.error('[MarketModel] Error stack:', error.stack);
+    throw error;
+  }
 };
 
 export const insertDataInDB = async (data) => {
